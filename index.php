6c39d32f448114f75efe16183562f0155de2b5fa
zhangyueguo <1156391296@qq.com>
2015-10-05 13:41:37 +0800

    youxiugai

--- index.php
+++ index.php
@@ -3,3 +3,6 @@
  echo "<hr />";
  echo "wo shi jinan ren zai jinan gongzuo wo ai jinan ni  shi nali ren  a";
  $arr=array('shijie','nihao','zhang','yue','guo');
+ foreach($arr as $v){
+	 echo $v;
+ }
